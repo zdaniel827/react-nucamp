fa3c45482cf2012e359574a9c61315d03e3c7e56
Zachary Daniel <zdaniel827@gmail.com>
2020-01-13 22:51:33 -0800

    added prettier & controlled form to contact us page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Directory from "./DirectoryComponent";
-import CampsiteInfo from "./CampsiteInfoComponent"
+import CampsiteInfo from "./CampsiteInfoComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
@@ -12,50 +13,69 @@ import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            comments: COMMENTS,
-            partners: PARTNERS,
-            promotions: PROMOTIONS
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+      comments: COMMENTS,
+      partners: PARTNERS,
+      promotions: PROMOTIONS
+    };
+  }
 
-    render() {
+  render() {
     const HomePage = () => {
-        return (
-            <Home
-                campsites={this.state.campsites.filter(campsite => campsite.featured)[0]}
-                promotions={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                partners={this.state.partners.filter(partner => partner.featured)[0]}
-            />
-        );
-    }
-        
+      return (
+        <Home
+          campsites={
+            this.state.campsites.filter(campsite => campsite.featured)[0]
+          }
+          promotions={
+            this.state.promotions.filter(promotion => promotion.featured)[0]
+          }
+          partners={this.state.partners.filter(partner => partner.featured)[0]}
+        />
+      );
+    };
+
     const CampsiteWithId = ({ match }) => {
-        return (
-            <CampsiteInfo
-                campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-            />
-        )
-    }
-        
+      return (
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              campsite => campsite.id == match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            comment => comment.campsiteId === +match.params.campsiteId
+          )}
+        />
+      );
+    };
+
     return (
-        <div>
-            <Header />
-            <Switch>
-                <Route path='/home' component={HomePage} />
-                <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
-                <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                <Route exact path='/contactus' component={Contact} /> 
-                <Redirect to='/home' />
-            </Switch>
-            <Footer />
-        </div>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/directory"
+            render={() => <Directory campsites={this.state.campsites} />}
+          />
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
+          <Route
+            exact
+            path="/aboutus"
+            render={() => <About partners={this.state.partners} />}
+          />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
     );
-    }
+  }
 }
 
 export default Main;
