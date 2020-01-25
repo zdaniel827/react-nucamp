7f24c8b5a9d51ed89493bde3a942e0bb18d3151f
Zachary Daniel <zdaniel827@gmail.com>
2020-01-24 17:44:22 -0800

    fetch post

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -36,7 +36,7 @@ class CommentForm extends Component {
 
   handleSubmit = values => {
     this.toggleModal();
-    this.props.addComment(
+    this.props.postComment(
       this.props.campsiteId,
       values.rating,
       values.name,
@@ -161,7 +161,7 @@ function RenderSelectedCampsite({ campsite }) {
   return <div />;
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -181,7 +181,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
             </div>
           );
         })}
-        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
     );
   }
@@ -229,7 +229,7 @@ function CampsiteInfo(props) {
           <RenderSelectedCampsite campsite={props.campsite} />
           <RenderComments
             comments={props.comments}
-            addComment={props.addComment}
+            postComment={props.postComment}
             campsiteId={props.campsite.id}
           />
         </div>
